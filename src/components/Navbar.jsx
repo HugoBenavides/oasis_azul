e2f29227f5f25f4e459fdd94753d8fc717d2909d
Hugo Benavides <the.another.gerf@gmail.com>
2023-10-19 12:00:53 -0600

    Se agrega libreria de React-Icons para usar dentro de proyecto. Se continua trabajando en Navbar, faltan los estilos y el menú responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {BsPerson} from 'react-icons/bs'
+import {BiSearch} from 'react-icons/bi'
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Hello World</h1>
+    <div className='flex justify-between items-center'>
+      <div>
+        <h1>Albercas Oasis Azul</h1>    
+      </div>
+      <ul className='flex'>
+        <li>Químicos</li>
+        <li>Equipos</li>
+        <li>Mantenimiento</li>
+        <li>Construcción</li>
+        <li>Cotización</li>
+      </ul>
+      <div className='flex'>
+        <BsPerson className='icon' />
+        <BiSearch className='icon' />
+      </div>
     </div>
   )
 }
