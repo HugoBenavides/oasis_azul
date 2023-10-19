5fe0c849c4aacc055b307e4d4fb472836f784a60
Hugo Benavides <the.another.gerf@gmail.com>
2023-10-19 11:03:04 -0600

    Se instala React-Router-Dom para el manejo SPA dentro del proyecto, se pruega con la pagina Home y funciona sin problemas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './index.css'
-import Navbar from './components/Navbar'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+
 
 function App() {
 
   return (
     <div>
-      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </div>
   )
 }
