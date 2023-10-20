121275ef9b4323ecfe68fe7636c4891a171e56cc
Hugo Benavides <the.another.gerf@gmail.com>
2023-10-19 18:51:30 -0600

    Se termina de hacer la funcionalidad del menú movil junto con sus estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {BsPerson} from 'react-icons/bs'
 import {BiSearch} from 'react-icons/bi'
+import {AiOutlineClose} from 'react-icons/ai'
+import {HiOutlineMenuAlt4} from 'react-icons/hi'
+import {FaFacebook,FaTwitter,FaInstagram,FaPinterest, FaYoutube} from 'react-icons/fa'
 
 const Navbar = () => {
+
+  const [nav,setNav] = useState(false);
+  const [logo,setLogo] = useState(false);
+
+  const handleNav = ()=>{
+    setNav(!nav)
+    setLogo(!logo)
+  }
+
   return (
-    <div className='flex justify-between items-center'>
+    <div className='flex justify-between items-center h-20 px-4'>
       <div>
-        <h1>Albercas Oasis Azul</h1>    
+        {logo ? <></> : <h1>Albercas Oasis Azul</h1>   }
       </div>
-      <ul className='flex'>
+      <ul className='hidden md:flex'>
         <li>Químicos</li>
         <li>Equipos</li>
         <li>Mantenimiento</li>
         <li>Construcción</li>
         <li>Cotización</li>
       </ul>
-      <div className='flex'>
-        <BsPerson className='icon' />
-        <BiSearch className='icon' />
+      <div className='hidden md:flex'>
+        <BiSearch size={20} />
+        <BsPerson size={20} />
+      </div>
+      {/*Hamburguer*/}
+      <div onClick={handleNav} className='md:hidden z-10'>
+        {nav ? <AiOutlineClose size={20} /> : <HiOutlineMenuAlt4 size={20} />    }
+        
+      </div>
+      {/*Mobile*/}
+      <div className={nav ? 'absolute top-0 left-0 w-full bg-gray-100/90 px-4 flex flex-col' : 'absolute left-[-100%]' }>
+        <ul>
+          <h1>Albercas Oasis Azul</h1>
+          <li className='border-b-2 border-blue-800'>Químicos</li>
+          <li className='border-b-2 border-blue-800'>Equipos</li>
+          <li className='border-b-2 border-blue-800'>Mantenimiento</li>
+          <li className='border-b-2 border-blue-800'>Construcción</li>
+          <li className='border-b-2 border-blue-800'>Cotización</li>
+          <div className='flex flex-col'>
+            <button className='my-2'>Search</button>
+            <button className='mb-2'>Account</button>
+          </div>
+          <div className='flex justify-between'>
+            <FaFacebook className='icon' />
+            <FaTwitter className='icon' />
+            <FaYoutube className='icon' />
+            <FaInstagram className='icon' />
+            <FaPinterest className='icon' />
+          </div>
+        </ul>
       </div>
     </div>
   )
