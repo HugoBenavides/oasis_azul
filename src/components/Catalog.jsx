dd90212dafc097c8fba4ee6e476094b35a462a79
Hugo Benavides <the.another.gerf@gmail.com>
2023-10-30 17:45:07 -0600

    Se crea CatalgoCard.jsx para eficientar el código dentro de Catalog al hacer un componente que ayude a reducir la cantidad de líneas de código

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -5,47 +5,17 @@ import solar from '../assets/solarAlt2.jpg'
 import caldera from '../assets/caldera.jpg'
 import jacuzzi from '../assets/jacuzzi.jpg'
 import arena from '../assets/arena_silica.jpg'
+import CatalogCard from './CatalogCard'
 
 const Catalog = () => {
   return (
     <div className='max-w-[1240px] mx-auto px-4 py-16 grid grid-cols-2 lg:grid-cols-3 gap-4'>
-        <div className='relative cursor-pointer transition'>
-            <img className='rounded-md w-full h-full' src={cubierta} alt="cubierta" />
-            <div className='bg-gray-800/30 text-white md:bg-transparent md:hover:bg-gray-800/30 absolute w-full h-full top-0 left-0 rounded-md md:text-transparent md:hover:text-white'>
-                <p className='font-bold left-4 bottom-4 text-xl md:text-2xl absolute'>Cubiertas</p>
-            </div>
-        </div>
-        <div className='relative cursor-pointer transition'>
-            <img className='rounded-md w-[390px] h-[260px]' src={manguera} alt="accesorios" />
-            <div className='bg-gray-800/30 text-white md:bg-transparent md:hover:bg-gray-800/50 absolute w-full h-full top-0 left-0 rounded-md md:text-transparent md:hover:text-white'>
-                <p className='font-bold left-4 bottom-4 text-xl md:text-2xl absolute'>Accesorios para limpieza</p>
-            </div>
-        </div>
-        <div className='relative cursor-pointer transition'>
-            <img className='rounded-md w-[390px] h-[260px]' src={solar} alt="calentador" />
-            <div className='bg-gray-800/30 text-white md:bg-transparent md:hover:bg-gray-800/50 absolute w-full h-full top-0 left-0 rounded-md md:text-transparent md:hover:text-white'>
-                <p className='font-bold left-4 bottom-4 text-xl md:text-2xl absolute'>Calentadores solares</p>
-            </div>
-        </div>
-        <div className='relative cursor-pointer transition'>
-            <img className='rounded-md w-[390px] h-[260px]' src={caldera} alt="caldera" />
-            <div className='bg-gray-800/30 text-white md:bg-transparent md:hover:bg-gray-800/50 absolute w-full h-full top-0 left-0 rounded-md md:text-transparent md:hover:text-white'>
-                <p className='font-bold left-4 bottom-4 text-xl md:text-2xl absolute'>Caldera para Alberca</p>
-            </div>
-        </div>
-        <div className='relative cursor-pointer transition'>
-            <img className='rounded-md w-[390px] h-[260px]' src={jacuzzi} alt="jacuzzi" />
-            <div className='bg-gray-800/30 text-white md:bg-transparent md:hover:bg-gray-800/50 absolute w-full h-full top-0 left-0 rounded-md md:text-transparent md:hover:text-white'>
-                <p className='font-bold left-4 bottom-4 text-xl md:text-2xl absolute'>Construcción de Jacuzzi</p>
-            </div>
-        </div>
-        <div className='relative cursor-pointer transition'>
-            <img className='rounded-md w-[390px] h-[260px]' src={arena} alt="arena" />
-            <div className='bg-gray-800/30 text-white md:bg-transparent md:hover:bg-gray-800/50 absolute w-full h-full top-0 left-0 rounded-md md:text-transparent md:hover:text-white'>
-                <p className='font-bold left-4 bottom-4 text-xl md:text-2xl absolute'>Arena 
-                sílica para filtro</p>
-            </div>
-        </div>
+        <CatalogCard bg={cubierta} alt={"cubierta"} text={"Cubierta para Alberca"} />
+        <CatalogCard bg={manguera} alt={"accesorios"} text={"Accesorios para limpieza"} />
+        <CatalogCard bg={solar} alt={"panel"} text={"Calentador solar para piscina"} />
+        <CatalogCard bg={caldera} alt={"caldera"} text={"Caldera para albercas"} />
+        <CatalogCard bg={jacuzzi} alt={"jacuzzi"} text={"Construccion de jacuzzi"} />
+        <CatalogCard bg={arena} alt={"cubierta"} text={"Arena Sílica para filtro"} />
     </div>
   )
 }
--- src/components/CatalogCard.jsx
+++ src/components/CatalogCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const CatalogCard = () => {
+const CatalogCard = ({bg,alt,text}) => {
   return (
     <div className='relative cursor-pointer transition'>
-        <img className='rounded-md w-[390px] h-[260px]' src={arena} alt="arena" />
-        <div className='bg-gray-800/30 text-white md:bg-transparent md:hover:bg-gray-800/50 absolute w-full h-full top-0 left-0 rounded-md md:text-transparent md:hover:text-white'>
-            <p className='font-bold left-4 bottom-4 text-xl md:text-2xl absolute'>Arena sílica para filtro</p>
+        <img className='rounded-md w-[390px] h-[260px]' src={bg} alt={alt} />
+        <div className='bg-gray-800/30 text-white md:bg-transparent md:hover:bg-gray-800/70 absolute w-full h-full top-0 left-0 rounded-md md:text-transparent md:hover:text-white transition'>
+            <p className='font-bold left-4 bottom-4 text-xl md:text-2xl absolute'>{text}</p>
         </div>
     </div>
   )
